75c2019a7c4ee334542390853f63deb8ebf7c205
yosef <yosfemyayu@gmail.com>
2024-07-22 17:38:38 +0300

    the power of spreed operator

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,31 @@ import Like from "./components/Like";
 function App() {
   const [showAlert, setShowAlert] = useState(false);
   const [liked, setLiked] = useState(false);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "mosh",
+      age: 36,
+    },
+  });
   return (
     <>
+      {console.log(game)}
       <Like onclick={() => setLiked(!liked)} liked={liked}></Like>
-
       {showAlert && <Alert onclose={() => setShowAlert(false)}></Alert>}
       <Button color="success" onclick={() => setShowAlert(true)}>
         click me
+      </Button>
+      <Button
+        color="primary"
+        onclick={() => {
+          setGame({
+            ...game,
+            player: { ...game.player, name: "yosef the emyayu" },
+          });
+        }}
+      >
+        change name
       </Button>
     </>
   );
