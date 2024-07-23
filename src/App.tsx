e2dbb9848de68f9d7c876ee57342d725f3aa49bc
yosef <yosfemyayu@gmail.com>
2024-07-23 13:53:36 +0300

    updating nested state object

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-import Like from "./components/Like";
 
-function App() {
-  const [showAlert, setShowAlert] = useState(false);
-  const [liked, setLiked] = useState(false);
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "mosh",
-      age: 36,
+const App = () => {
+  const [customer, setCustomer] = useState({
+    name: "john",
+    address: {
+      city: "San Francisco",
+      zipCode: 1111,
     },
   });
+
   return (
     <>
-      {console.log(game)}
-      <Like onclick={() => setLiked(!liked)} liked={liked}></Like>
-      {showAlert && <Alert onclose={() => setShowAlert(false)}></Alert>}
-      <Button color="success" onclick={() => setShowAlert(true)}>
-        click me
-      </Button>
-      <Button
-        color="primary"
-        onclick={() => {
-          setGame({
-            ...game,
-            player: { ...game.player, name: "yosef the emyayu" },
-          });
-        }}
+      {console.log(customer.address.zipCode)}
+      <button
+        onClick={() =>
+          setCustomer({
+            ...customer,
+            address: { ...customer.address, zipCode: 2222 },
+          })
+        }
       >
-        change name
-      </Button>
+        add to cart
+      </button>
     </>
   );
-}
+};
 
 export default App;
