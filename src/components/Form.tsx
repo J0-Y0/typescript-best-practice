4b112105aa9883838ed2068eaf19c849a6011a31
yosef <yosfemyayu@gmail.com>
2024-07-25 15:28:39 +0300

    validation with useform hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,48 +1,58 @@
-import React, { ChangeEvent, FormEvent, useRef, useState } from "react";
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
-  const handleSubmit = (event: ChangeEvent<HTMLInputElement>) => {
-    setPerson({
-      ...person,
-      [event.target.name]: event.target.value,
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
 
   return (
-    <form
-      action=""
-      onSubmit={(event) => {
-        event.preventDefault();
-        console.log(person);
-      }}
-    >
+    <form action="" onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor="name" className="form-label">
-        Name
+        Full name
       </label>
       <input
-        value={person.name}
-        onChange={handleSubmit}
-        type="text"
-        name="name"
+        {...register("name", { required: true, minLength: 5 })}
         id="name"
+        type="text"
         className="form-control"
       />
+      {errors.name?.type === "minLength" && (
+        <label htmlFor="age" className="form-label">
+          invalid name, too small
+        </label>
+      )}
+      {errors.name?.type === "required" && (
+        <label htmlFor="age" className="form-label">
+          name is required
+        </label>
+      )}
       <label htmlFor="age" className="form-label">
-        age
+        Your age
       </label>
+
       <input
-        value={person.age}
-        onChange={handleSubmit}
+        {...register("age", { required: true, minLength: 5 })}
         type="number"
-        className="form-control"
-        name="age"
         id="age"
+        className="form-control"
       />
-      <input type="submit" value="submit" className="btn btn-primary mt-2" />
+      {errors.age?.type === "minLength" && (
+        <label htmlFor="age" className="form-label">
+          invalid age, too small
+        </label>
+      )}
+      {errors.age?.type === "required" && (
+        <label htmlFor="age" className="form-label">
+          required
+        </label>
+      )}
+      <input type="submit" value="submit" className="btn btn-primary my-2" />
     </form>
   );
 };
