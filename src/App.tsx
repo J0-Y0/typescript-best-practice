bf629ed9e35edd762a6abfcf62044df5c779db77
yosef <yosfemyau@gmail.com>
2024-07-27 15:33:38 +0300

    adding a loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,42 @@
 import { useEffect, useState } from "react";
 import axios, { CanceledError } from "axios";
-interface User{
-  id:number,
-  name: String,
-  email:String
+interface User {
+  id: number;
+  name: String;
+  email: String;
 }
 const App = () => {
-  const [users, setUsers] = useState<User[]>([])
-  const[error,setError] = useState('')
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const controller = new AbortController()
+    const controller = new AbortController();
+    setLoading(true);
     axios
       .get("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setLoading(false);
+        setUsers(res.data);
+      })
       .catch((err) => {
+
         if (err instanceof CanceledError) return;
         setError(err.message);
-      });
-    
-    return ()=>controller.abort()
-  },[]);
+                // setLoading(false);
+
+      })
+      // .finally(() => setLoading(false));
+
+    return () => controller.abort();
+  }, []);
   return (
     <>
+      {loading&& <div className="spinner-border">
+        </div>
+      }
       {error && <p className="text-danger">{error}</p>}
       <table className="table">
         <thead>
