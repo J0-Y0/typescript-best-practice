8019b8374b4d2ea18eb4d600a73953a7254399a9
yosef <yosfemyau@gmail.com>
2024-07-27 12:22:40 +0300

    simple axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { useState } from "react";
-import Form from "./components/Form";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+interface User{
+  id:number,
+  name: String,
+  email:String
+}
 const App = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  },[]);
   return (
     <>
-      <Form />
+      <table className="table">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user =><tr>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+            )
+          }
+        </tbody>
+      </table>
     </>
   );
 };
